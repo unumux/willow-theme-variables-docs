306c60aaed4c0e088489aa3c151c066ac4a4296a
Carl Henderson <chuckhendo@gmail.com>
2017-10-15 10:54:47 -0400

    making more readable with the power of tables

--- generate/client/App.js
+++ generate/client/App.js
@@ -1,36 +1,53 @@
-const React = require("react");
+import React from "react";
+import styled from "styled-components";
+
 
 export class App extends React.Component {
     render() {
         const filteredData = this.props.data.filter(item => Object.keys(item.variables).length > 0);
 
         return (
-            <div>
+            <table>
+                <style dangerouslySetInnerHTML={{__html: `
+                    h3 {
+                        margin-bottom: 0;
+                    }
+
+                    th, td {
+                        padding: 5px;
+                    }
+
+                    th {
+                        background: #eee;
+                        text-align: right;
+                    }
+
+                    .filename {
+                        background: #ddd;
+                        text-align: center;
+                    }
+                `}}/>
                 {filteredData.map((item) => {
-                    return (
-                        <div>
-                            <h3>{item.filename.replace("./node_modules/@unumux/", "")}</h3>
-                            {renderVariables(item.variables)}
-                        </div>
-                    )
+                    return [
+                        <tr>
+                            <th className="filename" colSpan="3"><h3>{item.filename.replace("./node_modules/@unumux/theme-coloniallife-default/variables/", "")}</h3></th>
+                        </tr>,
+                        renderVariables(item.variables)
+                    ];
                 })}
-            </div>
+            </table>
         );
     }
 }
 
 function renderVariables(variables) {
-    return (
-        <ul>
-            {Object.keys(variables).map((key) => {
-                const value = variables[key];
-                return (
-                    <li>
-                        <strong>{key}: </strong>
-                        {typeof value === 'string' ? value : renderVariables(value)}
-                    </li>
-                )
-            })}
-        </ul>
-    );
+    return Object.keys(variables).map((key) => {
+        const value = variables[key];
+        return (
+            <tr>
+                <th>{key}: </th>
+                <td>{typeof value === 'string' ? value : <table>{renderVariables(value)}</table>}</td>
+            </tr>
+        )
+    })
 }
